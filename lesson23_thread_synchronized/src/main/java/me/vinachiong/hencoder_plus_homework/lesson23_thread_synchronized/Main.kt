d56fb54c3ec2192317f7f2ea69a1b8f57e94c907
Vina Chiong <vina.chiong@gmail.com>
2019-06-25 10:23:03 +0800

    add module lesson 23

---
+++ lesson23_thread_synchronized/src/main/java/me/vinachiong/hencoder_plus_homework/lesson23_thread_synchronized/Main.kt
@@ -0,0 +1,20 @@
+package me.vinachiong.hencoder_plus_homework.lesson23_thread_synchronized
+
+
+fun main(args: Array<String>) {
+    // 1. 开启线程的三种方式，
+    //  - 线程相关的wait(), sleep()的区别
+    //  - run()和start()方法区别
+
+
+    // 2. 线程同步 关键字 @Synchronized,
+    // - 方法锁、类锁、对象锁
+    // - 对象锁 和 类锁是否会互相作用
+    // - 线程内存，共享内存
+
+    // 3. 线程同步 关键字 @Volatile
+
+    // 4. 死锁
+    // - 什么是死锁，写一个例子
+    // -
+}
---
+++ lesson23_thread_synchronized/src/main/java/me/vinachiong/hencoder_plus_homework/lesson23_thread_synchronized/TreadCreation.kt
@@ -0,0 +1,14 @@
+package me.vinachiong.hencoder_plus_homework.lesson23_thread_synchronized
+
+/**
+ *
+ *
+ * @author changweiliang@kungeek.com
+ * @version v1.2.0
+ */
+class TreadCreation {
+
+
+
+
+}
